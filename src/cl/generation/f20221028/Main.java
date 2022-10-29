ff64ad3c0f30e8325c998723f4468790ca07ef48
KiaraMimbela <kiaramimbelavasquez@gmail.com>
2022-10-29 20:17:59 -0300

    Uso objeto (estudiante)

--- src/cl/generation/f20221028/Main.java
+++ src/cl/generation/f20221028/Main.java
@@ -6,63 +6,69 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		capturarEstudiantes();
+	}
+	
+	public static void capturarEstudiantes() {
 		// Instanciar despues a estudiante
-		/*Estudiante estudiante = new Estudiante("Matthew", "Mimbela", "234567-2");
-		System.out.println(estudiante.toString());
-		estudiante.setEdad(3);
-		estudiante.setCorreo("piatun_azul@gmail.com");
-		estudiante.setCurso("5a");
-		System.out.println(estudiante.toString());
-		 */
-		Scanner sc = new Scanner(System.in);
-		//arreglo de estudiantes
-		ArrayList<Estudiante> estudiantes = new ArrayList<Estudiante>();
-		//en las arraylist se pueden poner los objetos como tipo de dato
-		//cuando no se sepa que objeto se usa, puede poner object
-		//en este caso se usa Estudiante
-		
-		//variable de opcion
-		int continuar = 2;
-		
-		do {
-			System.out.println("Ingrese el nombre del estudiante");
-			String nombre = sc.nextLine();
-			System.out.println("Ingrese el apellido del estudiante");
-			String apellido = sc.nextLine();
-			//instancia al objeto estudiante
-			Estudiante estudiante = new Estudiante();
-			//asignando valor al atributo
-			estudiante.setNombre(nombre);
-			estudiante.setApellido(apellido);
-			//agregando un objeto al arreglo
-			estudiantes.add(estudiante);
-			//pregunta
-			System.out.println("¿Quiere agregar otro estudiante");
-			System.out.println("(1) SI  -  (2)NO");
-			//capturando datos de la respuesta
-			continuar = sc.nextInt();
-			sc.nextLine();//corrige el error de capturar un numero y despues solicitar un nextline
-			
-		}while(continuar == 1);
-		
+				/*Estudiante estudiante = new Estudiante("Matthew", "Mimbela", "234567-2");
+				System.out.println(estudiante.toString());
+				estudiante.setEdad(3);
+				estudiante.setCorreo("piatun_azul@gmail.com");
+				estudiante.setCurso("5a");
+				System.out.println(estudiante.toString());
+				 */
+				Scanner sc = new Scanner(System.in);
+				//arreglo de estudiantes
+				ArrayList<Estudiante> estudiantes = new ArrayList<Estudiante>();
+				//en las arraylist se pueden poner los objetos como tipo de dato
+				//cuando no se sepa que objeto se usa, puede poner object
+				//en este caso se usa Estudiante
+				
+				//variable de opcion
+				int continuar = 2;
+				
+				do {
+					System.out.println("Ingrese el nombre del estudiante");
+					String nombre = sc.nextLine();
+					System.out.println("Ingrese el apellido del estudiante");
+					String apellido = sc.nextLine();
+					//instancia al objeto estudiante
+					Estudiante estudiante = new Estudiante();
+					//asignando valor al atributo
+					estudiante.setNombre(nombre);
+					estudiante.setApellido(apellido);
+					//agregando un objeto al arreglo
+					estudiantes.add(estudiante);
+					//pregunta
+					System.out.println("¿Quiere agregar otro estudiante");
+					System.out.println("(1) SI  -  (2)NO");
+					//capturando datos de la respuesta
+					continuar = sc.nextInt();
+					sc.nextLine();//corrige el error de capturar un numero y despues solicitar un nextline
+					
+				}while(continuar == 1);
+				
+				sc.close();
+				recorrerArreglo(estudiantes);
+	}
+
+	public static void recorrerArreglo(ArrayList<Estudiante> lista_estudiantes) {
 		System.out.println("----------------------------");
 		System.out.println("  DATOS DE LOS ESTUDIANTES");
 		System.out.println("----------------------------");
 		//imprimiendo el arreglo
 		//Tipo de dato  variable (singular de la arraylist) : arraylist
-		for (Estudiante estudiante : estudiantes) {
+		for (Estudiante estudiante : lista_estudiantes) {
 			System.out.println("Nombre : " + estudiante.getNombre());
 			System.out.println("Apellido : " + estudiante.getApellido());
 			System.out.println("----------------------------");
 		}
 
 		//Obtener un estudiante especifico por posicion
-		System.out.println(estudiantes.get(1));
+		System.out.println(lista_estudiantes.get(1));
 		
 		//Accedemos a un atributo del objeto
-		System.out.println(estudiantes.get(1).getNombre());
-		
-		
+		System.out.println(lista_estudiantes.get(1).getNombre());
 	}
-
 }
